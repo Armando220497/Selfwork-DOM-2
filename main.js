3badfe7d00517ac3b6dd7f32a54b2d6d20795ace
Armando Di Santo <armando.di.santo@hotmail.com>
2024-07-16 16:13:19 +0200

    Selfwork DOM 2

---
+++ main.js
@@ -0,0 +1,26 @@
+function creaArticolo() {
+
+    const titolo = document.getElementById('titolo').value;
+    const paragrafo = document.getElementById('paragrafo').value;
+
+    // Controlla se i campi sono vuoti
+    if (titolo.trim() === '' || paragrafo.trim() === '') {
+        alert('Per favore, compila sia il titolo che il paragrafo.');
+        return;
+    }
+
+    // Data
+    let date = new Date();
+    let formatDate = date.toLocaleDateString();
+
+    // Crea articolo
+    articolo.innerHTML = `
+    <p class="date">Data di pubblicazione: ${formatDate}</p>
+    <h2>${titolo}</h2>
+    <p>${paragrafo}</p>
+`;
+
+    // Pulisci gli input
+    document.getElementById('titolo').value = '';
+    document.getElementById('paragrafo').value = '';
+}
